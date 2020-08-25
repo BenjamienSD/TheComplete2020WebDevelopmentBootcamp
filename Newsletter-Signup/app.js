03c89300c2096e4d517ee01f8e4f4ba6bad9bbe2
BenjamienSD <benjamien.deprez.dtp@gmail.com>
2020-08-25 17:01:28 +0200

    init newsletter signup app

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const https = require('https');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from root');
+  res.sendFile(__dirname + '/signup.html');
 });
 
 app.listen(3000, () => {
