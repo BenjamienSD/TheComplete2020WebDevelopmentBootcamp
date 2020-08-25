b921746f87439957af6523b58aa9f233ea05535c
BenjamienSD <benjamien.deprez.dtp@gmail.com>
2020-08-25 15:18:01 +0200

    finishing simple weatherApp

---
+++ WeatherProject/app.js
@@ -0,0 +1,64 @@
+// init express and https
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// server GET request, send html
+app.get('/', (serverRequest, serverResponse) => {
+  serverResponse.sendFile(__dirname + '/index.html');
+});
+
+// server POST request, city & units
+app.post('/', (serverRequest, serverResponse) => {
+  const query = serverRequest.body.city;
+  const apiKey = 'd409b3b9c3ab29ade7b28427026fad7e';
+  const units = serverRequest.body.units;
+
+  // check for unit type
+  function checkUnit(units) {
+    if (serverRequest.body.units === 'imperial') {
+      unit = 'Fahrenheit';
+    } else {
+      unit = 'Celcius';
+    }
+  }
+
+  checkUnit();
+
+  // set url
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${units}&appid=${apiKey}`;
+
+  // https API GET request, handle API response
+  https.get(url, (apiResponse) => {
+    console.log(apiResponse.statusCode);
+
+    // response returns data (hex), use JSON.parse to convert to JSON, log the data
+    apiResponse.on('data', (data) => {
+      // declare constants
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const desc = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imgUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+      // handle server response
+      serverResponse.write(`<img src="${imgUrl}"/>`);
+      serverResponse.write(`<p>The weather is currently ${desc}</p>`);
+      serverResponse.write(
+        `<h1>The temperature in ${query} is ${temp} degrees ${unit}</h1>`
+      );
+      serverResponse.send();
+    });
+  });
+});
+
+/*
+  
+*/
+
+app.listen(3000, () => {
+  console.log('Running on port 3000...');
+});
---
+++ WeatherProject/notes.md
@@ -0,0 +1,43 @@
+# Weather API project
+
+## Making GET request with the Node HTTPS module
+
+Standard node library = https (native).  
+Alternatives: Axios, SuperAgent, Fetch, Got.
+
+```js
+const https = require('https');
+```
+
+`https.get(url[, options][, callback])`.
+
+In the callback we process the response using `response.on()`.  
+We can use the chrome plugin 'Awesome JSON Viewer' to easily deconstruct the object and extract the relevant data, like temp.
+
+```js
+// Server GET request, then handle the server response
+app.get('/', (serverRequest, serverResponse) => {
+  // set url
+  const url = 'https://api.openweathermap.org/d...';
+
+  // https API GET request, handle API response
+  https.get(url, (apiResponse) => {
+    console.log(apiResponse.statusCode);
+
+    // response returns data (hex), use JSON.parse to convert to JSON, log the data
+    apiResponse.on('data', (data) => {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      serverResponse.send('The temperature in Gent is ' + temp + '°C');
+    });
+  });
+});
+```
+
+Within a single GET method you can only do 1 res.send()
+
+## Using body-parser to parse POST requests to the server. (Getting data from user)
+
+```js
+const bodyParser = require('body-parser');
+```
