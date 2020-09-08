494fb2e02a4d88e5402d80047a4bf2c7cefba162
BenjamienSD <benjamien.deprez.dtp@gmail.com>
2020-09-08 14:15:24 +0200

    finishing challenge: add database to blog

--- blog/app.js
+++ blog/app.js
@@ -2,14 +2,12 @@
 // imports
 
 const express = require("express");
+const mongoose = require('mongoose')
 const bodyParser = require("body-parser");
 const date = require(__dirname + '/date.js')
 const _ = require('lodash')
-
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+require('dotenv').config()
+const url = process.env.mongoURI
 
 // app config 
 
@@ -20,21 +18,45 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const posts = []
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+// schema
+const postSchema = new mongoose.Schema({
+  day: String,
+  date: String,
+  title: String,
+  content: String
+})
+
+// model
+const Post = mongoose.model("Post", postSchema)
 
 // routing
 
 // home page
 app.get('/', (req, res) => {
-  res.render('home', { postItems: posts })
+  Post.find((err, posts) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('home', { postItems: posts })
+    }
+  })
 })
 
 // single post
 app.get('/posts/:postId', (req, res) => {
   // if posts array has an object with a title of req.params.postId console.log('match found')
-  posts.forEach(post => {
-    if (_.lowerCase(req.params.postId) === _.lowerCase(post.title)) {
-      res.render('post', { day: post.day, date: post.date, title: post.title, content: post.content })
+  const postId = req.params.postId
+
+  Post.findOne({ _id: postId }, (err, post) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('post', { title: post.title, day: post.day, date: post.date, content: post.content })
     }
   })
 })
@@ -59,15 +81,29 @@ app.get('/compose', (req, res) => {
 app.post('/compose', (req, res) => {
   const postDay = date.getDay()
   const postDate = date.getDate()
-  const newPost = { day: postDay, date: postDate, title: req.body.postTitle, content: req.body.postBody }
-  posts.push(newPost)
-  res.redirect('/')
+
+  const post = new Post({
+    day: postDay,
+    date: postDate,
+    title: req.body.postTitle,
+    content: req.body.postBody
+  })
+
+  // by putting this callback in the save method we make sure the post is saved before we redirect
+  post.save(err => {
+    if (!err) {
+      res.redirect('/')
+    }
+  })
+
 })
 
 // port config
 
-const port = 3000 || process.env.PORT
-
+let port = process.env.PORT || 3000
+if (port === null || port === '') {
+  port = 3000
+}
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
